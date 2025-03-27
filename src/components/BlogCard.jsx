baa91b2f850cb1a77af9cbd7a9a83058a0d7dd09
Zaryab Zubair <zaryabz043@gmail.com>
2025-03-27 14:35:43 +0500

    make card responsive

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -8,7 +8,7 @@ const BlogCard = ({ blog }) => {
       className="flex flex-col justify-center items-center gap-2 p-3 border border-slate-200 rounded-md cursor-pointer"
       to={`/blog/${blog._id}`}
     >
-      <div className="overflow-hidden">
+      <div className="overflow-hidden w-full h-[13rem]">
         <img
           src={blog?.coverImage}
           alt={blog?.title}
@@ -17,9 +17,13 @@ const BlogCard = ({ blog }) => {
         />
       </div>
       <div className="flex flex-col justify-start items-start gap-3 w-full">
-        <h1 className="text-xl font-bold capitalize">{blog?.title}</h1>
-        <div className="flex w-full justify-between items-center gap-2">
-          <p className="text-md font-bold">{blog?.createdBy?.name}</p>
+        <h1 className="text-xl font-bold capitalize  w-full truncate overflow-hidden whitespace-nowrap text-ellipsis">
+          {blog?.title}
+        </h1>
+        <div className="flex flex-col sm:flex-row items-start gap-0 w-full justify-between sm:items-center sm:gap-2">
+          <p className="text-md font-bold w-full truncate overflow-hidden whitespace-nowrap text-ellipsis">
+            {blog?.createdBy?.name}
+          </p>
           <p className="text-sm">{formatDate(blog?.createdAt)}</p>
         </div>
       </div>
