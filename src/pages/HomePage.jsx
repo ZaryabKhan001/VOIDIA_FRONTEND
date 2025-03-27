36ad20496b51812bb7c5855c57cd02132100f96f
Zaryab Zubair <zaryabz043@gmail.com>
2025-03-27 12:22:47 +0500

    change in frontend code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,8 @@ const HomePage = () => {
           <p>No blogs to show.</p>
         </div>
       ) : (
-        <div className="my-24 md:mx-10">
+        <div className="my-24 md:mx-10 flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">All Blogs</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {data?.map((blog, index) => (
               <BlogCard blog={blog} key={index} />
